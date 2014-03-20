72171afea6547a2b98ff8022c8c1e3453f2fed88
Kyle Oliveira <kyle.oliveira@cornell.edu>
2014-03-20 11:23:29 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Cucumber Salad
+### A (naive) way to parallelize Cucumber runs
+
+#### Process
+This script attempts to parallelize Cucumber runs by taking each grouping of
+tags, splitting them into nearly equal lists, then merging each list for the
+individual tags to produce a given number of equally long lists of tags with
+a similar mix of tag types in each list.
+
+These lists are then run in Cucumber as separate processes that produce JSON
+output. When each of these processes finish, an additional run for all Scenarios
+tagged with @nightly-jobs are run in series. This additional run produces an
+additional JSON file.
+
+Finally, all the JSON files are merged into a single result file.
+
+#### Requirements
+| Req | Description |
+| known_tags.csv | A file listing all grouping tags. If your Scenarios are stratified by speed somehow, use those tags. |
+| 'headless' profile | A Cucumber profile named 'headless'. You can certainly edit this in code. |
+
+#### Caveats
+* You'll need enough memory to run this stuff in parallel, of course.
---
+++ cucumber_salad.rb
@@ -0,0 +1,99 @@
+#!/usr/bin/ruby
+require 'rubygems'
+require 'json'
+
+class Array
+  def in_groups(num_groups)
+    return [] if num_groups == 0
+    slice_size = (self.size/Float(num_groups)).ceil
+    self.each_slice(slice_size).to_a
+  end
+end
+
+number_of_lists = 10
+
+
+tags = []
+begin
+  file = File.new('known_tags.csv', 'r')
+  while (line = file.gets)
+    tags << line.chomp
+  end
+  file.close
+rescue => err
+  puts "Exception: #{err}"
+  err
+end
+
+puts "Read in these tags:\n#{tags}"
+
+tags = Hash[
+  tags.map! do |tag|
+    find_result = `find /Users/kco26/RubymineProjects/kuality/kuality-kfs-cu/features -name "*feature" -exec grep -H #{tag} {} \\;`
+    [ tag, find_result.split("\n").collect {|s| s.split(' ')[1] }.uniq ]
+  end
+]
+
+puts "Converted to these lists:\n#{tags}"
+
+count = 0
+lists = Hash.new
+tags.each do |tag, vals|
+  vals.in_groups(number_of_lists).each do |s|
+    count += 1
+    lists[count].nil? ? lists.merge!({count => s.to_a}) : lists[count].push(s.to_a).flatten!
+  end
+  count = 0
+end
+puts "Here are our #{number_of_lists} lists:\n#{lists}"
+
+# Now create threads that run each of these lists in parallel, excluding @nightly-jobs tests
+threads = []
+lists.each do |id, list|
+  threads[id] = spawn 'cucumber /Users/kco26/RubymineProjects/kuality/kuality-kfs-cu/features' <<
+                              ' -p master --tags ~@nightly-jobs' <<
+                               list.map { |t| " --tags #{t}" }.join('') <<
+                              " --format json -o kuality-kfs-cu-output.split#{format('%#02d', id)}.json"
+end
+Process.waitall
+
+# Now run all nightly-jobs tests in serial
+#number_of_lists += 1
+#cmd = 'cucumber /Users/kco26/RubymineProjects/kuality/kuality-kfs-cu/features' <<
+#              ' -p master --tags @nightly-jobs --format json' <<
+#              " -o kuality-kfs-cu-output.split#{number_of_lists.format('%2d')}.json"
+#Process.wait(spawn cmd)
+
+# Merge all the results
+results = []
+(number_of_lists + 1).times do |x|
+  results << JSON.parse(File.read("kuality-kfs-cu-output.split#{format('%#02d', number_of_lists)}.json"))
+end
+
+final = []
+results.first.each_with_index do |ah,i|
+  results[1..(results.length - 1)].each do |b|
+    unless (bh = b[i])
+      bh = {}
+      puts "seems b has no #{i} key, merging skipped"
+    end
+
+    final << ah.merge(bh).inject({}) do |f, (k,v)|
+      if v.is_a?(String)
+        if v =~ /\A\d+\.\d+\Z/
+          v = v.to_f
+        elsif v =~ /\A\d+\Z/
+          v = v.to_i
+        end
+      end
+      f.update k => v
+    end
+  end
+end
+
+# Merge complete, let's output to a final file...
+File.open('kuality-kfs-cu-output.json', 'w') do |f|
+  f.write(final.to_json)
+end
+
+# Done!
